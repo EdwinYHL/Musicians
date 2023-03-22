b0b94d934f00d16800185ca707c9e5ee6dd58310
EdwinYHL <126517767+EdwinYHL@users.noreply.github.com>
2023-03-22 19:58:30 +0000

    new file:   Musicos/Musicos.csproj 	new file:   Musicos/Program.cs 	new file:   Musicos/bin/Debug/net7.0/Musicos 	new file:   Musicos/bin/Debug/net7.0/Musicos.deps.json 	new file:   Musicos/bin/Debug/net7.0/Musicos.dll 	new file:   Musicos/bin/Debug/net7.0/Musicos.pdb 	new file:   Musicos/bin/Debug/net7.0/Musicos.runtimeconfig.json 	new file:   Musicos/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   Musicos/obj/Debug/net7.0/Musicos.AssemblyInfo.cs 	new file:   Musicos/obj/Debug/net7.0/Musicos.AssemblyInfoInputs.cache 	new file:   Musicos/obj/Debug/net7.0/Musicos.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Musicos/obj/Debug/net7.0/Musicos.GlobalUsings.g.cs 	new file:   Musicos/obj/Debug/net7.0/Musicos.assets.cache 	new file:   Musicos/obj/Debug/net7.0/Musicos.csproj.AssemblyReference.cache 	new file:   Musicos/obj/Debug/net7.0/Musicos.csproj.CoreCompileInputs.cache 	new file:   Musicos/obj/Debug/net7.0/Musicos.csproj.FileListAbsolute.txt 	new file:   Musicos/obj/Debug/net7.0/Musicos.dll 	new file:   Musicos/obj/Debug/net7.0/Musicos.genruntimeconfig.cache 	new file:   Musicos/obj/Debug/net7.0/Musicos.pdb 	new file:   Musicos/obj/Debug/net7.0/apphost 	new file:   Musicos/obj/Debug/net7.0/ref/Musicos.dll 	new file:   Musicos/obj/Debug/net7.0/refint/Musicos.dll 	new file:   Musicos/obj/Musicos.csproj.nuget.dgspec.json 	new file:   Musicos/obj/Musicos.csproj.nuget.g.props 	new file:   Musicos/obj/Musicos.csproj.nuget.g.targets 	new file:   Musicos/obj/project.assets.json 	new file:   Musicos/obj/project.nuget.cache

---
+++ Musicos/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    class Musico
+    {
+        public string name{ get; set;}
+        public Musico(string n){name = n;}
+        public void Saluda(){Console.WriteLine("Hola soy: " + name);}
+        public virtual void Toca()
+        {Console.WriteLine($"{name} esta tocando su instrumento");}
+    }
+    class Baterista: Musico
+    {
+    public string bateria{get;set;}
+    public Baterista (string n, string b):base(n)
+    {bateria =b;}
+    public override void Toca()
+    {
+    Console.WriteLine($"{name} tocando su bateria");
+    }
+    }
+    class Bajista : Musico
+    {public string bajo{get;set;}
+     public Bajista (string n, string b):base(n)
+     {bajo =b;}
+        public override void Toca()
+        {
+            Console.WriteLine($"{name} tocando su bajo");
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        List<Musico> grupo = new List<Musico> ();
+        grupo.Add(new Bajista("Joe","Yamaha"));
+        //grupo.Add(new Musico("Joe","Yamaha"));
+        //grupo.Add(new Baterista("Joe","Yamaha"));
+        //grupo.Add(new Guitarrista("Joe","Yamaha"));    
+        foreach(var m in grupo)
+        {m.Saluda();}
+        foreach(var m in grupo)
+        {m.Toca();}
+    }
+}
