54fc7785fefd37ad6216dd3503ea062c2e4b2ec7
EdwinYHL <126517767+EdwinYHL@users.noreply.github.com>
2023-03-22 17:23:01 -0700

    Update Program.cs

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,43 +1,76 @@
-﻿internal class Program
+internal class Program
 {
-    class Musico
+    class Musician
     {
         public string name{ get; set;}
-        public Musico(string n){name = n;}
-        public void Saluda(){Console.WriteLine("Hola soy: " + name);}
-        public virtual void Toca()
-        {Console.WriteLine($"{name} esta tocando su instrumento");}
+        public Musician(string n){name = n;}
+        public void Greethin(){Console.WriteLine("Hello i'm: " + name);}
+        public virtual void Play()
+        {Console.WriteLine($"{name} my roll in thi's band is ");}
     }
-    class Baterista: Musico
+    class Singer: Musician
     {
-    public string bateria{get;set;}
-    public Baterista (string n, string b):base(n)
-    {bateria =b;}
-    public override void Toca()
-    {
-    Console.WriteLine($"{name} tocando su bateria");
-    }
-    }
-    class Bajista : Musico
-    {public string bajo{get;set;}
-     public Bajista (string n, string b):base(n)
-     {bajo =b;}
-        public override void Toca()
+        public string singer{get;set;}
+        public Singer (string n, string b):base(n)
+        {singer=b;}
+        public override void Play()
         {
-            Console.WriteLine($"{name} tocando su bajo");
+            Console.WriteLine($"{name} it's singing");
+        }
+    }
+    class Guitarist: Musician
+    {
+        public string guitar {get;set;}
+        public Guitarist(string n, string b): base(n)
+        {guitar=b;}
+        public override void Play()
+        {
+            Console.WriteLine($"{name} it's playing solo with the guitar");
+        }
+    }
+    class Pianist: Musician
+    {
+        public string piano{get;set;}
+        public Pianist(string n, string b):base(n)
+        {piano=b;}
+        public override void Play()
+        {
+            Console.WriteLine($"{name} it's playing solo with the piano");
+        }
+    }
+    class Drumer: Musician
+    {
+    public string drums{get;set;}
+    public Drumer (string n, string b):base(n)
+    {drums =b;}
+    public override void Play()
+    {
+    Console.WriteLine($"{name} it's playing solo with the drums");
+    }
+    }
+    class Bass : Musician
+    {public string bass{get;set;}
+     public Bass (string n, string b):base(n)
+     {bass =b;}
+        public override void Play()
+        {
+            Console.WriteLine($"{name} it's playing solo with the bass");
         }
     }
 
     private static void Main(string[] args)
     {
-        List<Musico> grupo = new List<Musico> ();
-        grupo.Add(new Bajista("Joe","Yamaha"));
-        //grupo.Add(new Musico("Joe","Yamaha"));
-        //grupo.Add(new Baterista("Joe","Yamaha"));
-        //grupo.Add(new Guitarrista("Joe","Yamaha"));    
-        foreach(var m in grupo)
-        {m.Saluda();}
-        foreach(var m in grupo)
-        {m.Toca();}
+        Console.WriteLine("With us Guns N' Roses");
+        List<Musician> GunsNRoses = new List<Musician> ();
+        GunsNRoses.Add(new Singer("Axl","Shure ULX2/58"));
+        GunsNRoses.Add(new Pianist("Axl","Baldwin Bp 165"));
+        GunsNRoses.Add(new Guitarist("Slash","Hunter Burst(Gibson Les Paul '59)"));
+        GunsNRoses.Add(new Guitarist("Izzy Stradlin","Gibson ES-175")); 
+        GunsNRoses.Add(new Bass("Duff","Fender Jazz Bass Special"));
+        GunsNRoses.Add(new Drumer("Adler","Tama Rockstar Custom Faux Rock"));   
+        foreach(var m in GunsNRoses)
+        {m.Greethin();}
+        foreach(var m in GunsNRoses)
+        {m.Play();}
     }
 }
